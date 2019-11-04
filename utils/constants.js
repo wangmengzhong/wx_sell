9acfbd56cc2479ad527752f2c05448c8cbfee02b
Administrator <2369915949@qq.com>
2019-11-04 17:17:47 +0800

    init

---
+++ utils/constants.js
@@ -0,0 +1,490 @@
+const constants = [
+  {
+    "id": "id1",
+    "name": "name1",
+    "group": [
+      {
+        "productId": 1,
+        "productName": "group1",
+        "num":0,
+      },
+      {
+        "productId": 2,
+        "productName": "group2",
+        "num": 0,
+      },
+      {
+        "productId": 3,
+        "productName": "group3",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id2",
+    "name": "name2",
+    "group": [
+      {
+        "productId": 4,
+        "productName": "group4",
+        "num": 0,
+      },
+      {
+        "productId": 5,
+        "productName": "group5",
+        "num": 0,
+      },
+      {
+        "productId": 6,
+        "productName": "group6",
+        "num": 0,
+      },
+      {
+        "productId": 7,
+        "productName": "group7",
+        "num": 0,
+      },
+      {
+        "productId": 8,
+        "productName": "group8",
+        "num": 0,
+      }, {
+        "productId": 9,
+        "productName": "group9",
+        "num": 0,
+      },
+      {
+        "productId": 10,
+        "productName": "group10",
+        "num": 0,
+      },
+      {
+        "productId": 11,
+        "productName": "group11",
+        "num": 0,
+      },
+      {
+        "productId": 12,
+        "productName": "group12",
+        "num": 0,
+      },
+      {
+        "productId": 13,
+        "productName": "group13",
+        "num": 0,
+      },
+      {
+        "productId": 14,
+        "productName": "group14",
+        "num": 0,
+      },
+      {
+        "productId": 15,
+        "productName": "group15",
+        "num": 0,
+      },
+      {
+        "productId": 16,
+        "productName": "group16",
+        "num": 0,
+      },
+    ]
+  },
+  {
+    "id": "id3",
+    "name": "name3",
+    "group": [
+      {
+        "productId": 17,
+        "productName": "group17",
+        "num": 0,
+      },
+      {
+        "productId": 18,
+        "productName": "group18",
+        "num": 0,
+      },
+      {
+        "productId": 19,
+        "productName": "group19",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id4",
+    "name": "name4",
+    "group": [
+      {
+        "productId": 20,
+        "productName": "group20",
+        "num": 0,
+      },
+      {
+        "productId": 21,
+        "productName": "group21",
+        "num": 0,
+      },
+      {
+        "productId": 22,
+        "productName": "group22",
+        "num": 0,
+      },
+      {
+        "productId": 23,
+        "productName": "group23",
+        "num": 0,
+      },
+      {
+        "productId": 24,
+        "productName": "group24",
+        "num": 0,
+      },
+      {
+        "productId": 25,
+        "productName": "group25",
+        "num": 0,
+      },
+      {
+        "productId": 26,
+        "productName": "group26",
+        "num": 0,
+      },
+      {
+        "productId": 27,
+        "productName": "group27",
+        "num": 0,
+      },
+      {
+        "productId": 28,
+        "productName": "group28",
+        "num": 0,
+      },
+      {
+        "productId": 29,
+        "productName": "group29",
+        "num": 0,
+      },
+      {
+        "productId": 30,
+        "productName": "group30",
+        "num": 0,
+      },
+      {
+        "productId": 31,
+        "productName": "group31",
+        "num": 0,
+      },
+      {
+        "productId": 32,
+        "productName": "group32",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id5",
+    "name": "name5",
+    "group": [
+      {
+        "productId": 33,
+        "productName": "group33",
+        "num": 0,
+      },
+      {
+        "productId": 34,
+        "productName": "group34",
+        "num": 0,
+      },
+      {
+        "productId": 35,
+        "productName": "group35",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id6",
+    "name": "name6",
+    "group": [
+      {
+        "productId": 36,
+        "productName": "group36",
+        "num": 0,
+      },
+      {
+        "productId": 37,
+        "productName": "group37",
+        "num": 0,
+      },
+      {
+        "productId": 38,
+        "productName": "group38",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id7",
+    "name": "name7",
+    "group": [
+      {
+        "productId": 39,
+        "productName": "group39",
+        "num": 0,
+      },
+      {
+        "productId": 40,
+        "productName": "group40",
+        "num": 0,
+      },
+      {
+        "productId": 41,
+        "productName": "group41",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id8",
+    "name": "name8",
+    "group": [
+      {
+        "productId": 42,
+        "productName": "group42",
+        "num": 0,
+      },
+      {
+        "productId": 43,
+        "productName": "group43",
+        "num": 0,
+      },
+      {
+        "productId": 44,
+        "productName": "group44",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id9",
+    "name": "name9",
+    "group": [
+      {
+        "productId": 45,
+        "productName": "group45",
+        "num": 0,
+      },
+      {
+        "productId": 46,
+        "productName": "group46",
+        "num": 0,
+      },
+      {
+        "productId": 47,
+        "productName": "group47",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id10",
+    "name": "name10",
+    "group": [
+      {
+        "productId": 48,
+        "productName": "group48",
+        "num": 0,
+      },
+      {
+        "productId": 49,
+        "productName": "group49",
+        "num": 0,
+      },
+      {
+        "productId": 50,
+        "productName": "group50",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id11",
+    "name": "name11",
+    "group": [
+      {
+        "productId": 51,
+        "productName": "group51",
+        "num": 0,
+      },
+      {
+        "productId": 52,
+        "productName": "group52",
+        "num": 0,
+      },
+      {
+        "productId": 53,
+        "productName": "group53",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id12",
+    "name": "name12",
+    "group": [
+      {
+        "productId": 54,
+        "productName": "group54",
+        "num": 0,
+      },
+      {
+        "productId": 55,
+        "productName": "group55",
+        "num": 0,
+      },
+      {
+        "productId": 56,
+        "productName": "group56",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id13",
+    "name": "name13",
+    "group": [
+      {
+        "productId": 57,
+        "productName": "group57",
+        "num": 0,
+      },
+      {
+        "productId": 58,
+        "productName": "group58",
+        "num": 0,
+      },
+      {
+        "productId": 59,
+        "productName": "group59",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id14",
+    "name": "name14",
+    "group": [
+      {
+        "productId": 60,
+        "productName": "group60",
+        "num": 0,
+      },
+      {
+        "productId": 61,
+        "productName": "group61",
+        "num": 0,
+      },
+      {
+        "productId": 62,
+        "productName": "group62",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id15",
+    "name": "name15",
+    "group": [
+      {
+        "productId": 63,
+        "productName": "group63",
+        "num": 0,
+      },
+      {
+        "productId": 64,
+        "productName": "group64",
+        "num": 0,
+      },
+      {
+        "productId": 65,
+        "productName": "group65",
+        "num": 0,
+      }
+    ],
+  },
+  {
+    "id": "id16",
+    "name": "name16",
+    "group": [
+      {
+        "productId": 66,
+        "productName": "group66",
+        "num": 0,
+      },
+      {
+        "productId": 67,
+        "productName": "group67",
+        "num": 0,
+      },
+      {
+        "productId": 68,
+        "productName": "group68",
+        "num": 0,
+      },
+      {
+        "productId": 69,
+        "productName": "group69",
+        "num": 0,
+      },
+      {
+        "productId": 70,
+        "productName": "group70",
+        "num": 0,
+      },
+      {
+        "productId": 71,
+        "productName": "group71",
+        "num": 0,
+      },
+      {
+        "productId": 72,
+        "productName": "group72",
+        "num": 0,
+      },
+      {
+        "productId": 73,
+        "productName": "group73",
+        "num": 0,
+      },
+      {
+        "productId": 74,
+        "productName": "group74",
+        "num": 0,
+      },
+      {
+        "productId": 75,
+        "productName": "group75",
+        "num": 0,
+      },
+      {
+        "productId": 76,
+        "productName": "group76",
+        "num": 0,
+      },
+      {
+        "productId": 77,
+        "productName": "group77",
+        "num": 0,
+      },
+      {
+        "productId": 78,
+        "productName": "group78",
+        "num": 0,
+      }
+    ],
+  },
+]
+
+module.exports =  constants
+
